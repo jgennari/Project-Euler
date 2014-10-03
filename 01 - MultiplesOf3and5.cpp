61a20fa79ac21300b7d88071113f45e9d8fa7f76
Christina Gordon <Christina.Gordon@lennoxnas.com>
2014-10-03 14:48:25 -0400

    completed solution 1

--- 01 - MultiplesOf3and5.cpp
+++ 01 - MultiplesOf3and5.cpp
@@ -1,16 +1,22 @@
-#include <iostream>
 
+#include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
 
-	for (int i = 0; i < count; ++i)
+	int sum2= 0;
+	
+	for (int i = 1; i <1000 ; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
+		if (i % 3==0 || i % 5 ==0)
 		{
 
-
+		   sum2 +=i;
+		  
 		}
+    
 	}
+
+	cout<< "\n";
+	cout<< "sum of all the multiples of 3 or 5 below 1000: "<<sum2 <<"\n";
 }
